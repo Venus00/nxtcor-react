aef61ccf7e43c219d4c9334935f0bb6e09fef58e
jawadbrayer <jawadbrayer99@gmail.com>
2026-01-21 15:30:00 +0100

    default start on live

--- src/components/live/Video.tsx
+++ src/components/live/Video.tsx
@@ -670,8 +670,12 @@ const VideoStream: React.FC = () => {
             className=""
             allow="autoplay; fullscreen"
             style={{
-              width: "100%",
-              height: "100%",
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100vw',
+              height: '100vh',
+              zIndex: -1,
             }}
           />
         </div>
